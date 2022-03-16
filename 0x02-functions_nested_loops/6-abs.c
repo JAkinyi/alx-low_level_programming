37cd8bed23777e10df318ef7843962dfcb432c4e
JAkinyi <juneclaudine20@gmail.com>
2022-03-15 21:41:30 -0700

    add task 6

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _abs - print absolute value of number
+ * @n : number to be checked
+ * Return: absolute value
+ *
+ */
+int _abs(int n)
+{
+	if (n >= 0)
+	{
+		return (n);
+	}
+	else
+	{
+		return (-n);
+	}
+}
