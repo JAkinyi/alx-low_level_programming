c67f2bcd4a250969b97c58748786b303d5f00105
JAkinyi <juneclaudine20@gmail.com>
2022-03-15 21:25:54 -0700

    correct 3, betty doc style

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,8 +1,9 @@
 #include "main.h"
 
 /**
- * _islower - check for lowercase char 'c'
- * return: 1 or 0
+ * _islower - check for lowercase char
+ * @c : char to check
+ * Return: 1 or 0
  *
  */
 int _islower(int c)
