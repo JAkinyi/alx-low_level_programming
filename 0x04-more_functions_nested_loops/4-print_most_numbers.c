df51d6a3fcb9f7e9023c37469044bbb7748d83c3
JAO <63289276+JAkinyi@users.noreply.github.com>
2022-03-17 15:03:29 +0300

    Update 4-print_most_numbers.c

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -7,13 +7,13 @@
 void print_most_numbers(void)
 {
 	int num;
-
+	
 	for (num = 48; num <= 57; num++)
 	{
 		if ((num != 50) && (num != 52))
 		{
 			_putchar(num);
-		}	
+		}
 	}
 	_putchar('\n');
 }
