b986fbaed26f4158860c1d5651fa0127eb7695a2
JAkinyi <juneclaudine20@gmail.com>
2022-04-01 02:58:31 -0700

    function that computes natural squareroot of numbers

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+int sqr(int n, int i); 
+
+/**
+ * _sqrt_recursion - computes squareroot of an integer
+ * @n: int whose root is to be computed
+ * Return: natural squareroot or -1 where it doesnt exist
+ */
+
+int _sqrt_recursion(int n)
+{
+	if (n < 0)
+		return (-1);
+	return (sqr(n,1));
+}
+
+/**
+ * sqr - squares integers beginning from 1 and compares to n
+ * @n: number to be compared
+ * @i: iterating integers
+ * Return: iterator that is equal to the sqrt
+ */
+
+int sqr(int n, int i)
+{
+	if ((i * i) > n)
+		return (-1);
+	if ((i * i) == n)
+		return (i);
+	return (sqr(n, i + 1));
+}
