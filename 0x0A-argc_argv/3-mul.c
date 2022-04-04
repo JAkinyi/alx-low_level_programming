dfa391d07d471f3c73b8f77aa591b68c293e6266
JAkinyi <juneclaudine20@gmail.com>
2022-04-04 06:15:19 -0700

    multiply two integers passed as arguments to main

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - multiplies two numbers passed as arguments to main
+ * @argc: number of arguments passed
+ * @argv: array containing the arguments
+ * Return 1 or 0
+ */
+
+int main(int argc, char *argv)
+{
+	int mul;
+	
+	if (argc < 3 || argc > 3)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	mul = atoi(argv[1]) * atoi(argv[2]);
+
+	printf("%d\n", mul);
+	return (0);
+}
