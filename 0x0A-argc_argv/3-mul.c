6788f147de39fb9b1ba74e830ddbefd531ee6895
JAkinyi <juneclaudine20@gmail.com>
2022-04-04 06:28:58 -0700

    betty error, return and takinga care of an unused variable

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -5,13 +5,15 @@
  * main - multiplies two numbers passed as arguments to main
  * @argc: number of arguments passed
  * @argv: array containing the arguments
- * Return 1 or 0
+ * Return: 1 or 0
  */
 
 int main(int argc, char **argv)
 {
 	int mul;
 	
+	(void)argc;
+
 	if (argc < 3 || argc > 3)
 	{
 		printf("Error\n");
