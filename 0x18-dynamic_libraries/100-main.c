3a35fd72f85f2e0028609a7ba371acb05d9dfe17
JAkinyi <juneclaudine20@gmail.com>
2022-06-17 08:35:33 -0700

    dynamic lib accessible with python

---
+++ 0x18-dynamic_libraries/100-main.c
@@ -0,0 +1,27 @@
+#include "main2.h"
+
+int add(int a, int b)
+{
+	int c = a + b;
+	return(c);
+}
+int sub(int a, int b)
+{
+	int c = a - b;
+	return(c);
+}
+int mul(int a, int b)
+{
+	int c = a * b;
+	return(c);
+}
+int div(int a, int b)
+{
+	int c =a / b;
+	return(c);
+}
+int mod(int a, int b)
+{
+	int c = a % b;
+	return(c);
+}
---
+++ 0x18-dynamic_libraries/main2.h
@@ -0,0 +1,12 @@
+#ifndef MAIN2_H
+#define MAIN2_H
+
+#include <stdio.h>
+
+int add(int a, int b);
+int sub(int a, int b);
+int mul(int a, int b);
+int div(int a, int b);
+int mod(int a, int b);
+
+#endif /* MAIN2_H */
